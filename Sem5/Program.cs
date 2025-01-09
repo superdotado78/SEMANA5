0fae84c1262dbbcaa0d87f88cf4dd4d391524b5c
Jorge Diaz mancheno <jl.diazm@uea.edu.ec>
2025-01-08 22:11:23 -0500

    https://github.com/superdotado78/Estructura-de-datos.git

---
+++ Sem5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Lista de asignaturas
+        List<string> asignaturas = new List<string>
+        {
+            "Matemáticas", "Física", "Química", "Historia", "Lengua"
+        };
+
+        // Mostrar mensaje para cada asignatura
+        foreach (var asignatura in asignaturas)
+        {
+            Console.WriteLine($"Yo estudio {asignatura}");
+        }
+    }
+}
